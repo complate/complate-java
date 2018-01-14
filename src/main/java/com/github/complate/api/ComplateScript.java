6a23dc868e6f9918da96aa5701277accaa2a2246
Michael Vitz <michael.vitz@innoq.com>
2018-01-14 15:50:20 +0100

    Enhance documentation of ComplateScript

--- src/main/java/com/github/complate/api/ComplateScript.java
+++ src/main/java/com/github/complate/api/ComplateScript.java
@@ -5,6 +5,16 @@ import java.io.InputStream;
 
 /**
  * IO Abstraction for a JavaScript script that is used by complate.
+ * <p>The script is required to declare a function named <strong>render</strong>
+ * that takes the arguments <strong>stream</strong>, <strong>tag</strong> and
+ * <strong>params</strong>. The following code snippet shows an simple example:
+ * <pre>{@code
+ * function render(stream, tag, params) {
+ *   stream.writeln(tag);
+ *   stream.writeln(params);
+ *   stream.flush();
+ * }
+ * }</pre>
  *
  * @author Michael Vitz
  * @since 0.1.0
@@ -12,7 +22,7 @@ import java.io.InputStream;
 public interface ComplateScript {
 
     /**
-     * Return an {@link InputStream} for the content of an underlying script.
+     * Return an {@link InputStream} for the content of the underlying script.
      * <p>It is expected that each call creates a <i>fresh</i> stream.
      *
      * @return the input stream for the underlying script (must not be {@code null})
